69785e398c7beb4d3b47b8f9d948d97a5fda1eef
thesuss <lullisuss@gmail.com>
2016-09-28 17:02:13 +0200

    added create action in menu controller

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -5,4 +5,9 @@ class MenuController < ApplicationController
   def new
     @menu = Menu.new
   end
+
+  def create
+    @menu = menu.create(menu_params)
+    flash[:notice] = "Successfully added menu"
+  end
 end
