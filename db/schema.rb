9924d0ba8ae91c16d8a5fd41d28f76e7ce9e6379
thesuss <lullisuss@gmail.com>
2016-09-29 16:54:54 +0200

    ran rails db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160929130341) do
 
   # These are extensions that must be enabled in order to support this database
@@ -32,7 +31,7 @@ ActiveRecord::Schema.define(version: 20160929130341) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
-  
+
   create_table "shopping_cart_items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
