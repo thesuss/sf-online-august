7930d183228f12600cc6a2305bb9422ca3209901
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-09-29 15:08:14 +0200

    Created shopping cart item table and migrated

--- app/models/shopping_cart_item.rb
+++ app/models/shopping_cart_item.rb
@@ -1,3 +1,7 @@
 class ShoppingCartItem < ActiveRecord::Base
   acts_as_shopping_cart_item
+
+create_table :cart_items do |t|
+  t.shopping_cart_item_fields # Creates the cart items fields
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928094103) do
+ActiveRecord::Schema.define(version: 20160929130341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 20160928094103) do
     t.text     "dish_ingredients"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "shopping_cart_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shopping_carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
