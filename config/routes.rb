80404c218ac700a6ea1d0598279735efb38ccc8c
thesuss <lullisuss@gmail.com>
2016-09-30 09:55:35 +0200

    sorted problem with migrations of acts as shoppable

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get '/restaurant', controller: :restaurant, action: :index
+  get '/carts', controller: :carts, action: :index
 
   resources :menu, only: [:index, :create, :new]
   resources :dishes, only: [:new, :show, :create] do
     post 'add_item', controller: :carts, action: :add_item
   end
 
-  get '/menu', controller: :menu, action: :index
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929130341) do
+ActiveRecord::Schema.define(version: 20160930074546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,15 @@ ActiveRecord::Schema.define(version: 20160929130341) do
   end
 
   create_table "shopping_cart_items", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.integer  "quantity"
+    t.integer  "item_id"
+    t.string   "item_type"
+    t.integer  "price_cents",    default: 0,     null: false
+    t.string   "price_currency", default: "USD", null: false
   end
 
   create_table "shopping_carts", force: :cascade do |t|
