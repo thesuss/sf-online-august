86d93ed952e616e728d710abc4364ae5a408202b
Amber <amber@amberwilkie.com>
2016-09-30 12:59:13 +0200

    Update unit test for dish-menu association

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,7 +3,5 @@ class Dish < ApplicationRecord
   validates_presence_of :dish_desc
   validates_presence_of :dish_price
 
-  # has_many :menus
-  # belongs_to :menu
   has_and_belongs_to_many :menus
 end
