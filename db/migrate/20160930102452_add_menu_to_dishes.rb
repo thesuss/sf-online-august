bc4797b1b45cbe6e0f8996ff70c78160ab4dd900
Amber <amber@amberwilkie.com>
2016-09-30 12:56:04 +0200

    Remove empty migrations

--- db/migrate/20160930102452_add_menu_to_dishes.rb
+++
@@ -1,5 +0,0 @@
-class AddMenuToDishes < ActiveRecord::Migration[5.0]
-  def change
-    # add_reference :dishes, :menu, foreign_key: true
-  end
-end
--- db/migrate/20160930103122_add_dishes_to_menu.rb
+++
@@ -1,5 +0,0 @@
-class AddDishesToMenu < ActiveRecord::Migration[5.0]
-  def change
-    # add_reference :menus, :dish, foreign_key: true
-  end
-end
