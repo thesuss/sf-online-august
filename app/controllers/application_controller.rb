39a6b9f720e7267586ffcd60ad4abaec5c23636e
thesuss <lullisuss@gmail.com>
2016-10-02 10:03:50 +0200

    removed sign out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,4 @@ class ApplicationController < ActionController::Base
     store_location_for(:user, request.url)
   end
 
-  def after_sign_out_path_for(resource)
-    request.referrer || root_path
-  end
-
 end
