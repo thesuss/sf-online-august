e47d912389a9662a251cab493419e5e47688cb69
thesuss <lullisuss@gmail.com>
2016-10-02 10:06:12 +0200

    refactored old syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :store_current_location, :unless => :devise_controller?
+  before_filter :store_current_location, unless: :devise_controller?
 
   private
   def store_current_location
