96f8de88e28a37eb45350cf95e4b895e60c9f850
thesuss <lullisuss@gmail.com>
2016-10-05 11:53:40 +0200

    merged with upstream

--- features/step_definitions/dish_steps.rb
+++ features/step_definitions/dish_steps.rb
@@ -11,3 +11,12 @@ When(/^visit the "([^"]*)" menu page$/) do |menu_name|
   menu = Menu.find_by(title: menu_name)
   visit menu_path(menu)
 end
+Given(/^I am on the dish page for "([^"]*)"$/) do |arg1|
+  dish = Dish.find_by(dish_name: name)
+  visit dish_path(dish)
+end
+
+Then(/^I should be on the edit dish page for "([^"]*)"$/) do |arg1|
+  dish = Dish.find_by(dish_name: name)
+  expect(current_path).to eq edit_dish_path(dish)
+end
