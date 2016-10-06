4b0e7732b1b8e63168357440637d678bcb990fa4
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-10-06 11:41:49 +0200

    Added category to params, functional in server now

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -52,6 +52,6 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :description, :street, :zipcode, :town)
+    params.require(:restaurant).permit(:name, :category, :description, :street, :zipcode, :town)
   end
 end
