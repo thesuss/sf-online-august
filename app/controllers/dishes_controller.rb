e54cce85a4c9f1eff931d0906f33a3ea2ae7d571
MikaelFeher <mikael.feher@gmail.com>
2016-10-06 12:52:39 +0200

    Refactors feature test

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,6 +1,5 @@
 class DishesController < ApplicationController
-  before_action :authenticate_user!,
-                :find_dish_from_params, only: [:show, :edit, :update]
+  before_action :find_dish_from_params, only: [:show, :edit, :update]
 
   load_and_authorize_resource
   before_action :owner_has_restaurant?, only: :new
