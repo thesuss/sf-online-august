a4df4f01f1fa822374cf55ae3ea2cc428190dc29
thesuss <lullisuss@gmail.com>
2016-10-10 15:51:07 +0200

    Response gives expected response apart from datetime

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RestaurantsController < ApplicationController
   def index
-    @collection = Restaurant.all
-    render json: { restaurants: @collection }
+    @restaurants = Restaurant.all
+    render json: { restaurants: @restaurants }
   end
 end
--- spec/requests/api/v1/restaurants_spec.rb
+++ spec/requests/api/v1/restaurants_spec.rb
@@ -1,13 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::RestaurantsController, type: :request do
+  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+
   describe 'GET /v1/restaurants' do
+
+    let!(:owner) { create(:user, email: 'whatever@random_restaurant.com', role: 'owner') }
+    let!(:restaurant) { create(:restaurant, user: owner) }
+
     it 'should return json with restaurants' do
       get '/api/v1/restaurants'
-
       json_response = JSON.parse(response.body)
       expect(response.status).to eq 200
-      expect(json_response['restaurants']).to eq 'Lots of stuff'
+      expect(json_response['restaurants']).to eq [{"id"=>1,
+                                                   "name"=>"MyString",
+                                                   "description"=>"MyText",
+                                                   "user_id"=>1,
+                                                   "created_at"=>"2016-10-10T13:40:08.716Z",
+                                                   "updated_at"=>"2016-10-10T13:40:08.716Z",
+                                                   "street"=>"Fjällgatan 3",
+                                                   "zipcode"=>41463,
+                                                   "town"=>"Gothenburg",
+                                                   "latitude"=>57.696531,
+                                                   "longitude"=>11.9448777,
+                                                   "category"=>"Thai"}]
+
     end
+
   end
+
 end
