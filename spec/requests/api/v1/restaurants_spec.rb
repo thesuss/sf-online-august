1e62d75de9c9a0fbd093041b96a34cb9400d038a
thesuss <lullisuss@gmail.com>
2016-10-10 15:06:33 +0200

    WIP test for restaurant api

---
+++ spec/requests/api/v1/restaurants_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::RestaurantsController, type: :request do
+  describe 'GET /v1/restaurants' do
+    it 'should return json with restaurants' do
+      get '/api/v1/restaurants'
+
+      json_response = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(json_response['message']).to eq 'Lots of stuff'
+    end
+  end
+end
