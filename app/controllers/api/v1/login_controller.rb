4770e9b031030099d8285f9cd0baa936e9af9680
Amber <amber@amberwilkie.com>
2016-10-13 19:06:41 +0200

    Trying different rack-cors things to get login working

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::LoginController < DeviseTokenAuth::SessionsController
-  protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  protect_from_forgery with: :null_session
 end
--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module SlowFood
         origins '*'
         resource '*', headers: :any,
                       methods: [:get, :post],
+                      expose: %w(access-token expiry token-type uid client format),
                       max_age: 0
       end
     end
