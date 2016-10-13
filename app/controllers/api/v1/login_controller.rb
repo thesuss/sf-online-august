e83ebf7f696ff7824e91bdfff05767c248e9e674
Amber <amber@amberwilkie.com>
2016-10-13 18:51:48 +0200

    Get devise token thing out of the application controller

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,3 +1,4 @@
 class Api::V1::LoginController < DeviseTokenAuth::SessionsController
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  protect_from_forgery with: :null_session
+  include DeviseTokenAuth::Concerns::SetUserByToken
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   include CanCan::ControllerAdditions
   protect_from_forgery with: :exception
   before_action :store_current_location, unless: :devise_controller?
-  include DeviseTokenAuth::Concerns::SetUserByToken
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
