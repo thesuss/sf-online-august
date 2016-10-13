7ee99264f596af1a3f8dc8fee657f37d68463209
Amber <amber@amberwilkie.com>
2016-10-13 16:03:52 +0200

    Add devise token auth columns to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009095311) do
+ActiveRecord::Schema.define(version: 20161013135832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,9 @@ ActiveRecord::Schema.define(version: 20161009095311) do
     t.text     "address",                                     null: false
     t.datetime "created_at",                                  null: false
     t.datetime "updated_at",                                  null: false
+    t.string   "provider",               default: "email",    null: false
+    t.string   "uid",                    default: "",         null: false
+    t.text     "tokens"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
