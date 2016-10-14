5c5121d4bb745fb6b414115bb1698058e3edfe3c
blokkinen <viktoria_blok@hotmail.com>
2016-10-14 11:36:50 +0200

    Added the dish id and refactored the test

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::CartsController < ApplicationController
 
 
   def create
-    #binding.pry
     dish = Dish.find(params[:dish_id])
     cart = ShoppingCart.create
     cart.add dish, dish.price
-    render json: ({cart_id: cart.id, dishes: cart.shopping_cart_items})
+    #binding.pry
+    render json: ({cart_id: cart.id, dish_id: cart.shopping_cart_items[0].item.id})
   end
 end
