089c2fd79317a8745cd8617c25e5e2468c1bd3bb
Raoul DIFFOUO <diraulo@gmail.com>
2016-10-14 12:06:14 +0200

    [hotfix] add column type to migration

--- db/migrate/20161006152332_add_default_category_to_restaurants.rb
+++ db/migrate/20161006152332_add_default_category_to_restaurants.rb
@@ -1,5 +1,5 @@
 class AddDefaultCategoryToRestaurants < ActiveRecord::Migration[5.0]
   def change
-    change_column :restaurants, :category, default: 'Other'
+    change_column :restaurants, :category, :string, default: 'Other'
   end
 end
